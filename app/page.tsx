3b72d22eeb0ee052a5dff72c48611e3e7c9170b5
dillonpw <dillonpwalsh10@gmail.com>
2024-11-23 09:47:55 -0500

    updated dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 export default function Home() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      className="m-4 text-4xl"
-    >
-      testing
-    </motion.div>
+    <>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="m-4 text-4xl"
+      >
+        testing
+      </motion.p>
+    </>
   );
 }
